bfa9a68700eab6fb05593413fa9a1fb9365a5423
Poomainthan M <86851654+poomainthan@users.noreply.github.com>
2024-04-01 16:15:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,28 +33,28 @@ function App() {
   return (
     <div className="max-w-4xl m-auto relative">
       <header className={`${scrolling ? ' border-b border-gray-900' : ''}  fixed left-0 bg-black right-0 top-0 z-20`} id="home">
-        <div className="container m-auto px-4 py-6 max-w-4xl bg-black">
-          <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
+        <div className="container m-auto px-4 py-6 max-w-8xl bg-black">
+          <div className="flex flex-col gap-4 sm:flex-row justify-between items-center ">
             <div>
-              {/*
-              <h1 className="font-bold text-2xl ">Poomainthan M</h1>
-              */}
+              {
+              <h1 className="font-bold text-2xl ">PORTFOLIO</h1>
+              }
             </div>
             
             <div>
               <ul className="flex gap-4">
                 <li>
-                  <a href="#projects" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a href="#projects" className="text-gray-300 hover:text-white cursor-pointer">
                     Projects
                   </a>
                 </li>
                 <li>
-                  <a href="#technologies" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a href="#technologies" className="text-gray-300 hover:text-white cursor-pointer">
                     Technologies
                   </a>
                 </li>
                 <li>
-                  <a href="#aboutme" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a href="#aboutme" className="text-gray-300 hover:text-white cursor-pointer">
                     About me
                   </a>
                 </li>
